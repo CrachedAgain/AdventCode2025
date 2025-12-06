f9666061b2cd288bb9993f73c3e59e80a674cc7f
CrachedAgain <37341700+CrachedAgain@users.noreply.github.com>
2025-12-06 22:34:02 +0100

    First test Bad IDs part 2

---
+++ 04-badid2.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", async ()=>{
+    let elemData = document.getElementById("idListIDs");
+    let elemResults = document.getElementById("idResults");
+    let elemSumIDs = document.getElementById("idSumBadID");
+
+//    let r = await fetch("datafile-ID.txt");
+    let r = await fetch("testfile-ID.txt");
+    let data = await r.text();
+    let arrData = data.split(",");
+    elemData.innerText = arrData.join("\r\n");
+    let arrResults = [];
+    let numSumBadIDs = 0;
+
+    for (let i of arrData) {
+        let r = i.trim().split("-");
+        if (    r.length===2 && 
+                !isNaN(parseInt(r[0], 10)) && 
+                !isNaN(parseInt(r[1], 10)) ){
+            let start = parseInt(r[0]);
+            let end = parseInt(r[1]);
+            arrResults.push("Testing range: "+i.trim());
+            for ( let num=start;num <=end;num++){
+                let test = num.toString(10);
+                for ( let lenSlice=1; lenSlice*lenSlice <= test.length; lenSlice++){
+                    if (    test.length % lenSlice === 0 &&
+                            test.slice(0,lenSlice).repeat(test.length/lenSlice) === test){
+                        numSumBadIDs += num;
+                        arrResults.push("Bad ID: "+test);
+                        break;
+                    }
+                }
+            }
+        } else {
+            if ( i !== ""){
+                arrResults.push("Bad line: "+i.trim())
+            }
+        }
+    }
+    elemResults.innerText = arrResults.join("\r\n");
+    elemSumIDs.innerText = numSumBadIDs.toString();
+});
