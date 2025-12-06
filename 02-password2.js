6379acd100d30d8a8f46c13c79ea5d065251d01f
CrachedAgain <37341700+CrachedAgain@users.noreply.github.com>
2025-12-06 14:35:55 +0100

    Part 2 base files

---
+++ 02-password2.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", async ()=>{
+    let elemData = document.getElementById("idTextFile");
+    let elemResults = document.getElementById("idResults");
+    let elemassword = document.getElementById("idPassword");
+
+//    let r = await fetch("datafile.txt");
+    let r = await fetch("testfile.txt");
+    let data = await r.text();
+    elemData.innerText = data;
+
+    let arrData = data.split(/\r|\n|\r\n/);
+    let posLock = 50;
+    let reg = /^(L|R)([0-9]{1,})$/i;
+    let arrResults = ["The lock starts at "+ posLock.toString()];
+    let password = 0;
+
+    for (let i of arrData) {
+        let r = reg.exec(i);
+        if (r){
+            if ( r[1].toLowerCase()==="l"){
+                posLock = (posLock + 100 -  parseInt( r[2]) % 100) % 100;
+            } else {
+                posLock = (posLock + parseInt( r[2])) % 100;
+            }
+            arrResults.push("Command "+i+" moves the lock to " + posLock.toString() );
+            if ( posLock === 0){
+                password++;
+                arrResults.push("New password value: "+ password.toString())
+            }
+        } else {
+            if ( i !== ""){
+                arrResults.push("Bad line: "+i)
+            }
+        }
+    }
+    elemResults.innerText = arrResults.join("\r\n");
+    elemassword.innerText = password.toString();
+});
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This repo is for Advent of Code 2025.
 Here are the parts:
 
 - [Password](01-password.html)
+- [Password 2](02-password2.html)
