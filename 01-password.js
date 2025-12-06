a40cde72293b061f59330d95790c9b8c693bba36
CrachedAgain <37341700+CrachedAgain@users.noreply.github.com>
2025-12-06 13:49:19 +0100

    First test

--- 01-password.js
+++ 01-password.js
@@ -1,3 +1,37 @@
-window.addEventListener("load",()=>{
-    
+window.addEventListener("load", async ()=>{
+    let elemData = document.getElementById("idTextFile");
+    let elemResults = document.getElementById("idResults");
+    let elemassword = document.getElementById("idPassword");
+
+    let r = await fetch("datafile.txt");
+    let data = await r.text();
+    elemData.innerText = data;
+
+    let arrData = data.split(/\r|\n|\r\n/);
+    let posLock = 50;
+    let reg = /^(L|R)([0-9]{1,})$/ig;
+    let arrResults = ["The lock starts at "+ posLock.toString()];
+    let password = 0;
+
+    for (let i of arrData) {
+        let r = reg.exec(i);
+        if (r){
+            if ( r[1].toLowerCase()==="l"){
+                posLock = (posLock + 100 -  parseInt( r[2]) % 100) % 100;
+            } else {
+                posLock = (posLock + parseInt( r[2])) % 100;
+            }
+            arrResults.push("Command "+r[1]+" moves the lock to " + posLock.toString() );
+            if ( posLock === 0){
+                password++;
+                arrResults.push("New password valus: "+ password.toString())
+            }
+        } else {
+            if ( i !== ""){
+                arrResults.push("Bad line: "+i)
+            }
+        }
+    }
+    elemResults.innerText = arrResults.join("\r\n");
+    elemassword.innerText = posLock.toString();
 });
