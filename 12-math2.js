1d2368f0b4782323fae2a490a958ca1b480cde3d
CrachedAgain <37341700+CrachedAgain@users.noreply.github.com>
2025-12-10 22:16:58 +0100

    First actual test math 2

--- 12-math2.js
+++ 12-math2.js
@@ -8,17 +8,30 @@ window.addEventListener("load", async ()=>{
     let data = await r.text();
     let arrData = data.split(/\r|\n|\r\n/).filter( e=>e.trim()!=="" ).map( e=> e.split(""));
     elemData.innerText = arrData.map( e => e.join(" ")).join("\r\n");
-    debugger;
+
+    let arrOperations = arrData[arrData.length-1].filter( e=> e.trim());
+    let arrColNumbers = arrData.slice(0, arrData.length-1);
     let arrResults = [];
     let numTotals = 0;
 
-    for (let numProblem=0; numProblem < arrData[0].length; numProblem++){
-        let arrProblem = arrData.map( e => e[numProblem] );
-        let operation = arrProblem[ arrProblem.length-1 ];
+    let arrNumbers=[];
+    let arrAllNumbers=[];
+
+    for (let index=0; index < arrColNumbers[0].length; index++){
+        let number = arrColNumbers.filter( e => e[index].trim() ).join("");
+        if ( number === ""){
+            arrNumbers.reverse();
+            arrAllNumbers.push(arrNumbers);
+            arrNumbers = [];
+        } else {
+            arrNumbers.push(number);
+        }
+    }
+    for (let index=0; index < arrAllNumbers.length; index++){
+        let operation = arrOperations[ index ];
         let startValue = operation==="*" ? 1 : 0;
-        arrProblem = arrProblem.slice(0, arrProblem.length-1);
-        let numResult = arrProblem.reduce( (prev, cur)=> (operation==="*" ? prev*parseInt(cur) : prev+parseInt(cur)), startValue)
-        arrResults.push("Operation: "+ arrProblem.join(" "+operation+" ")+" = "+numResult.toString());
+        let numResult = arrAllNumbers[index].reduce( (prev, cur)=> (operation==="*" ? prev*parseInt(cur) : prev+parseInt(cur)), startValue)
+        arrResults.push("Operation: "+ arrAllNumbers[index].join(" "+operation+" ")+" = "+numResult.toString());
         numTotals += numResult;
     }
 
