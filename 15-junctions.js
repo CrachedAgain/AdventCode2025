2972e51aa83f60e425560170dd8699b84d06f852
CrachedAgain <37341700+CrachedAgain@users.noreply.github.com>
2025-12-13 23:35:55 +0100

    Re done structure

--- 15-junctions.js
+++ 15-junctions.js
@@ -6,8 +6,16 @@ window.addEventListener("load", async ()=>{
 //    let r = await fetch("datafile-junctions.txt");
     let r = await fetch("testfile-junctions.txt");
     let data = await r.text();
-    let arrData = data.split(/\r|\n|\r\n/).filter( e=>e.trim()!=="" ).map( e=> [e.split(",").map(e => parseInt(e,10))]);
-    elemData.innerText = arrData.map( e => e[0].join(",")).join("\r\n");
+    let arrData = data.split(/\r|\n|\r\n/).filter( e=>e.trim()!=="" ).map( (e)=> {
+        let p1 =  {
+            point: [e.split(",").map(e => parseInt(e,10))],
+            conn: [],
+            circuit: [],
+        };
+        p1.circuit.push(p1);
+        return p1;
+    });
+    elemData.innerText = arrData.map( e => e.point.join(",")).join("\r\n");
 
     let arrResults = [];
     let numTotals = 0;
@@ -22,29 +30,38 @@ window.addEventListener("load", async ()=>{
         let start = undefined;
         let end = undefined;
         for ( let numP1Row = 0; numP1Row < arrData.length; numP1Row++){
-            for ( let numP1Elem = 0; numP1Elem < arrData[numP1Row].length; numP1Elem++){
-                let p1 = arrData[numP1Row][numP1Elem];
-                for ( let numP2Row = numP1Row+1; numP2Row < arrData.length; numP2Row++){
-                    for ( let numP2Elem = 0; numP2Elem < arrData[numP2Row].length; numP2Elem++){
-                        let p2 = arrData[numP2Row][numP2Elem];
-                        if ( !minDistance || getDistance(p1,p2)<minDistance){
-                            minDistance = getDistance(p1,p2);
-                            start = [numP1Row,numP1Elem];
-                            end = [numP2Row,numP2Elem];
-                        }
-                    }
+            let p1 = arrData[numP1Row];
+            for ( let numP2Row = numP1Row+1; numP2Row < arrData.length; numP2Row++){
+                let p2 = arrData[numP2Row];
+                if ( !p1.conn.includes(p2) &&
+                    (!minDistance || 
+                    getDistance(p1.point,p2.point)<minDistance)){
+                    minDistance = getDistance(p1.point,p2.point);
+                    start = numP1Row;
+                    end = numP2Row;
                 }
             }
         }
-        arrResults.push("Connected: "+ arrData[start[0]][start[1]].join(",")+" with "+arrData[end[0]][end[1]]+" Distance: "+ minDistance.toString());
-        arrData[start[0]] = arrData[start[0]].concat( arrData[end[0]] );
-        arrData = arrData.slice(0,end[0]).concat( arrData.slice(end[0]+1,arrData.length));
+        arrResults.push("Connected: "+ arrData[start].point.join(",")+" with "+arrData[end].point+" Distance: "+ minDistance.toString());
+        arrData[start].conn.push( arrData[end] );
+        for ( let p of arrData[end].circuit){
+            if ( !arrData[start].circuit.includes(p)){
+                arrData[start].circuit.push(p);
+                p.circuit=arrData[start].circuit;
+            }
+        }
     }
-    arrData.sort( (a,b)=> b.length-a.length );
-    for (let  circuit of arrData){
-        arrResults.push( circuit.map( p => p.join(",") ).join(" , "));
+    let circuits = [];
+    for ( let point of arrData){
+        if (! circuits.includes(point.circuit)){
+            circuits.push(point.circuit);
+        }
     }
-    numTotals = arrData[0].length*arrData[1].length*arrData[2].length;
+    circuits.sort( (a,b)=> b.length-a.length );
+    for (let  circuit of circuits){
+        arrResults.push( circuit.map( p => p.point.join(",") ).join(" , "));
+    }
+    numTotals = circuits[0].length*circuits[1].length*circuits[2].length;
 
     elemResults.innerText = arrResults.join("\r\n");
     elemSumIDs.innerText = numTotals.toString();
