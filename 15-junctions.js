ffc3e9b800f94c317861a672c6dfdbac8700f176
CrachedAgain <37341700+CrachedAgain@users.noreply.github.com>
2025-12-13 22:02:15 +0100

    Typo and copy/paste

--- 15-junctions.js
+++ 15-junctions.js
@@ -13,7 +13,7 @@ window.addEventListener("load", async ()=>{
     let numTotals = 0;
 
     function getDistance(p1, p2){
-        return Math.sqrt( (p1[0]-p1[0])**2 + (p1[1]-p1[1])**2 + (p1[2]-p1[2])**2)
+        return Math.sqrt( (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
     }
 
     const maxConnections = 10;
